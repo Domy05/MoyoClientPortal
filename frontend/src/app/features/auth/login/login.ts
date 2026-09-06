551d2bd3ddb94bc7d572f205ba146ecb503a2a8b
Dominiqu <u24580482@tuks.co.za>
2026-09-06 23:10:25 +0200

    navigation bar implementation

--- frontend/src/app/features/auth/login/login.ts
+++ frontend/src/app/features/auth/login/login.ts
@@ -1,28 +1,29 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
   private fb = inject(FormBuilder);
   private router = inject(Router);
+  private authService = inject(AuthService);
 
   form = this.fb.group({
-    username: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
     password: ['', Validators.required],
+    rememberMe: [false],
   });
 
   submit() {
     if (this.form.invalid) return;
-    this.router.navigate(['/products']);
+    this.authService.login();
+    this.router.navigate(['/dashboard']);
   }
 }
