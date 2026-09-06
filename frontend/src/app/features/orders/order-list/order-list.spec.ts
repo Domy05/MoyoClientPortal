e049f995394034bf54345ca899c04ab498ed9449
Dominiqu <u24580482@tuks.co.za>
2026-09-06 20:08:35 +0200

    updated initial mock tests

--- frontend/src/app/features/orders/order-list/order-list.spec.ts
+++ frontend/src/app/features/orders/order-list/order-list.spec.ts
@@ -1,22 +1,18 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 import { OrderList } from './order-list';
 
 describe('OrderList', () => {
-  let component: OrderList;
-  let fixture: ComponentFixture<OrderList>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [OrderList],
+      providers: [provideRouter([])],
     }).compileComponents();
-
-    fixture = TestBed.createComponent(OrderList);
-    component = fixture.componentInstance;
-    await fixture.whenStable();
   });
 
   it('should create', () => {
+    const fixture = TestBed.createComponent(OrderList);
+    const component = fixture.componentInstance;
     expect(component).toBeTruthy();
   });
 });
